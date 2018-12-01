8b27e7fdbbb8fd7b593a143fa57418cdc35a773e
Hen Shalom <xdrakolax@gmail.com>
2018-12-01 21:27:03 +0200

    add material mui theme

--- src/index.js
+++ src/index.js
@@ -3,8 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import pink from '@material-ui/core/colors/pink';
+import green from '@material-ui/core/colors/green';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({
+    palette: {
+        primary: green,
+        secondary: pink,
+    },
+});
+ReactDOM.render(
+    <MuiThemeProvider theme={theme}>
+        <App />
+    </MuiThemeProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
