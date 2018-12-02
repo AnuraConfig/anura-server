321d55a50805ca5448896aceaa69dbfce1e8cab1
Hen Shalom <xdrakolax@gmail.com>
2018-12-02 19:08:13 +0200

    add main page and the list to the main page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from './components/Header/Header';
+import MainPage from './pages/MainPage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <MainPage />
       </div>
     );
   }
---
+++ src/pages/MainPage.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import ConfigList from '../components/ConfigLists/ConfigList'
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+    root: {
+        width: "80%",
+        margin: " 50px auto",
+        height: "100%"
+    },
+    grid: {
+        backgroundColor: theme.palette.background.paper,
+        flexGrow: 1,
+        width: "100%",
+    }
+})
+
+class MainPage extends React.Component {
+    render() {
+        const { classes } = this.props
+        return (
+            <Paper elevation={20} className={classes.root}>
+                <Grid container spacing={24} className={classes.grid} alignItems="stretch">
+                    <ConfigList />
+                </Grid>
+            </Paper>
+        )
+    }
+}
+
+export default withStyles(styles)(MainPage);
