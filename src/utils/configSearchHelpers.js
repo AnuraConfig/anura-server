34075ed5fa8c3d47c623abeaec0c62bacf9945ff
Hen Shalom <xdrakolax@gmail.com>
2018-12-03 23:54:58 +0200

    config search helper : re format file

--- src/utils/configSearchHelpers.js
+++ src/utils/configSearchHelpers.js
@@ -2,18 +2,18 @@ const NAME_MULTIPLAYER = 10;
 const DESCRIPTION_MULTIPLAYER = 1;
 
 export function configFilter(text) {
-    return function ({ name, description  }) {
+    return function ({ name, description }) {
         if (!text) return true
-        return (name.indexOf(text) !== -1 || description .indexOf(text) !== -1)
+        return (name.indexOf(text) !== -1 || description.indexOf(text) !== -1)
     }
 }
 
 function getItemScore(text, itemValue) {
     return itemValue.length - itemValue.indexOf(text)
 }
-function getScore(text, { name, description  }) {
+function getScore(text, { name, description }) {
     return NAME_MULTIPLAYER * getItemScore(text, name) +
-        DESCRIPTION_MULTIPLAYER * getItemScore(text, description )
+        DESCRIPTION_MULTIPLAYER * getItemScore(text, description)
 }
 
 export function configSort(text) {
