28803cd2a03fbea69e89ab48d6ebf49eea41bfca
Hen Shalom <xdrakolax@gmail.com>
2018-12-03 23:52:54 +0200

    add some more temp data

--- server/resolvers/configResolver.js
+++ server/resolvers/configResolver.js
@@ -31,6 +31,102 @@ export default function (obj, args) {
                     }]
                 }
             ]
+        },
+        {
+            name: "other service",
+            discretion: "some  otuer discretion for your services",
+            id: "123",
+            environments: [
+                {
+                    name: "production",
+                    configs: [
+                        {
+                            version: 1,
+                            data: JSON.stringify({
+                                url: "out234erUrl1.com",
+                            })
+                        },
+                        {
+                            version: 2,
+                            data: JSON.stringify({
+                                url: "ou434terUrl.com",
+                            })
+                        }]
+                },
+                {
+                    name: "test",
+                    configs: [{
+                        version: 1,
+                        data: JSON.stringify({
+                            url: "somn34eUrl.com",
+                        })
+                    }]
+                }
+            ]
+        },
+        {
+            name: "service name",
+            discretion: "some discretion for your services",
+            id: "123465",
+            environments: [
+                {
+                    name: "production",
+                    configs: [
+                        {
+                            version: 1,
+                            data: JSON.stringify({
+                                url: "outerUrl1.com",
+                            })
+                        },
+                        {
+                            version: 2,
+                            data: JSON.stringify({
+                                url: "outerUrl.com",
+                            })
+                        }]
+                },
+                {
+                    name: "test",
+                    configs: [{
+                        version: 1,
+                        data: JSON.stringify({
+                            url: "somneUrl.com",
+                        })
+                    }]
+                }
+            ]
+        },
+        {
+            name: "hen other service",
+            discretion: "some  otuer discretion for your services",
+            id: "123",
+            environments: [
+                {
+                    name: "production",
+                    configs: [
+                        {
+                            version: 1,
+                            data: JSON.stringify({
+                                url: "out234erUrl1.com",
+                            })
+                        },
+                        {
+                            version: 2,
+                            data: JSON.stringify({
+                                url: "ou434terUrl.com",
+                            })
+                        }]
+                },
+                {
+                    name: "test",
+                    configs: [{
+                        version: 1,
+                        data: JSON.stringify({
+                            url: "somn34eUrl.com",
+                        })
+                    }]
+                }
+            ]
         }
     ]
 }
