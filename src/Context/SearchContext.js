8fa301758801bd74eb787a378c42d9fb358e2460
Hen Shalom <xdrakolax@gmail.com>
2018-12-03 23:21:09 +0200

    add search context components

---
+++ src/Context/SearchContext.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { SearchContext } from './Contexts'
+
+export default class SearchContextProvider extends Component {
+    state = {
+        searchText: ""
+    }
+    changeText = (text) => this.setState({ searchText: text })
+
+    render() {
+        const value = {
+            text: this.state.searchText,
+            changeText: this.changeText
+        }
+        return (
+            <SearchContext.Provider value={value}>
+                {this.props.children}
+            </SearchContext.Provider>
+        )
+    }
+}
