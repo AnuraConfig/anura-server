daa7bc9dc756d6e4d5d44350a6e0a84c14badd21
Hen Shalom <xdrakolax@gmail.com>
2018-12-05 21:43:38 +0200

    add file vierer container

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import ConfigList from '../components/ConfigLists/ConfigList'
+import FileViewer from '../components/FileViewer/FileViewer'
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
@@ -25,6 +26,7 @@ class MainPage extends React.Component {
             <Paper elevation={20} className={classes.root}>
                 <Grid container spacing={24} className={classes.grid} alignItems="stretch">
                     <ConfigList />
+                    <FileViewer />
                 </Grid>
             </Paper>
         )
