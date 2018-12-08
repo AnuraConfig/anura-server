a2748572f9e7d45ed4b6c4bee221401f51336ccc
Hen Shalom <xdrakolax@gmail.com>
2018-12-08 17:26:35 +0200

    formated code

--- src/components/ConfigLists/AddItemListItem.js
+++ src/components/ConfigLists/AddItemListItem.js
@@ -4,8 +4,9 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
-
 import AddBox from '@material-ui/icons/AddBox';
+
+
 const styles = theme => ({
     listItem: {
         backgroundColor: theme.palette.primary.main,
