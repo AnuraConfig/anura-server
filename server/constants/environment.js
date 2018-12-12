1e1b5fc7905c05df9737626ca7d6cce9f8d314ad
Hen Shalom <xdrakolax@gmail.com>
2018-12-12 23:36:24 +0200

    add dotenv to env file

--- server/constants/environment.js
+++ server/constants/environment.js
@@ -1,4 +1,6 @@
-import managerTypes from './managerTypes'
+import * as managerTypes from './managerTypes'
+import dotenv from 'dotenv'
+dotenv.config();
 
 export const HUMAN_READABLE = process.env.HUMAN_READABLE || false
 export const DATA_MANAGER = process.env.DATA_MANAGER || managerTypes.FILE_SYSTEM
