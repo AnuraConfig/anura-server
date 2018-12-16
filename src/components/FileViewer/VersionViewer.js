a801845461be809f9785185852f696780f81aa44
Hen Shalom <xdrakolax@gmail.com>
2018-12-16 15:06:46 +0200

    add line space

--- src/components/FileViewer/VersionViewer.js
+++ src/components/FileViewer/VersionViewer.js
@@ -8,6 +8,7 @@ import Tab from '@material-ui/core/Tab';
 import JsonViewer from './JsonViewer';
 import Paper from '@material-ui/core/Paper/Paper';
 import { getMaxVersion, getMaxVersionIndex } from './VersionHelpers'
+
 const styles = theme => ({
     root: {
         flexGrow: 1,
