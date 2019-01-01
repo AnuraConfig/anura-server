c55755d7a002e9164fe9b5e693c58a7756a6b772
Hen Shalom <xdrakolax@gmail.com>
2019-01-02 00:10:55 +0200

    fix port constant and add static express

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ import cors from 'cors';
 import socketIo from 'socket.io';
 import { initializeSocket } from './stateManager/scoket'
 import stats from './routes/stats'
-import { PORT } from './constants/environment'
+import { SERVER_PORT } from './constants/environment'
 
 const server = new ApolloServer({
   typeDefs,
@@ -21,17 +21,20 @@ const httpServer = http.Server(app)
 const io = socketIo(httpServer)
 initializeSocket(io)
 
+app.use(express.static('build'))
+
 app.use(cors())
 app.use(bodyParser.json())
 app.use('/stats', stats)
 
 //is alive 
 app.get('/meaningOfLife', (req, res) => {
+  console.log('ping')
   res.send("42")
 })
 
 server.applyMiddleware({ app })
 
-httpServer.listen({ port: PORT }, () =>
-  console.log(`🚀 Server ready at http://localhost:4000/`)
+httpServer.listen({ port: SERVER_PORT }, () =>
+  console.log(`🚀 Server ready at http://localhost:${SERVER_PORT}/`)
 )
