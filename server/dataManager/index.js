66daa3f0b56e5c302932a088e08802d4a961e263
VainEyal <eyalvainer@gmail.com>
2019-01-04 23:53:08 +0200

    added mongo to the switch-case

--- server/dataManager/index.js
+++ server/dataManager/index.js
@@ -1,13 +1,16 @@
 import FileSystem from './FileSystem'
 import { DATA_MANAGER } from '../constants/environment'
 import * as managerTypes from '../constants/managerTypes'
+import MongoManager from './Mongo';
 
 function getDataManger() {
     switch (DATA_MANAGER.toUpperCase()) {
         case (managerTypes.FILE_SYSTEM):
             return new FileSystem();
+        case(managerTypes.MONGO):
+            return new MongoManager();
         default:
             return new FileSystem();
     }
 }
-export default getDataManger()
+export default getDataManger();
