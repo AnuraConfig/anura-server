fdc63a143046d7debcab20703f8e4792f3c2b7e0
Hen Shalom <xdrakolax@gmail.com>
2019-01-08 17:44:45 +0200

    fix config file reading

--- server/constants/configs.js
+++ server/constants/configs.js
@@ -1,10 +1,17 @@
 import YAML from 'yaml'
+import fs from 'fs'
+import { DEFAULT_CONFIG_LOCATION } from './constants'
 
-const defaultConfig = YAML.parse('../../defaultConfig.yaml')
+const file = fs.readFileSync(DEFAULT_CONFIG_LOCATION, 'utf8')
+const defaultConfig = YAML.parse(file)
 let config = defaultConfig
 
 function loadConfig(configFile, extraConfig) {
-    const configFileObject = YAML.parse(configFile)
+    let configFileObject = {}
+    if (extraConfig) {
+        const file = fs.readFileSync(configFile, 'utf8')
+        configFileObject = YAML.parse(file)
+    }
     config = Object.assign({}, defaultConfig, configFileObject, extraConfig)
     return config
 }
