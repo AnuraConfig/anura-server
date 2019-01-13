e77813ce40bc9323b4a783936538291f80f4fff4
VainEyal <eyalvainer@gmail.com>
2019-01-13 22:18:27 +0200

    service schema

--- server/dataManager/Mongo/schemas/index.js
+++ server/dataManager/Mongo/schemas/index.js
@@ -11,7 +11,15 @@ const EnviormentScehma = new mongoose.Schema({
     configIds: [{type: mongoose.Schema.Types.ObjectId, ref: 'configs'}]
 });
 
+const ServiceSchema = new mongoose.Schema({
+    _id: String,
+    name: String,
+    description: String,
+    enviormentIds: [{type: mongoose.Schema.Types.ObjectId, ref: 'envirorments'}]
+});
+
 module.exports = {
     Config: mongoose.model('configs', ConfigSchema),
-    Enviorment: mongoose.model('envirorments', EnviormentScehma)
+    Enviorment: mongoose.model('envirorments', EnviormentScehma),
+    Service: mongoose.model('services', ServiceSchema)
 };
