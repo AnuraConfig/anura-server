0a39b7bd4ac582a33cf842e91730dc4c2fc637cc
VainEyal <eyalvainer@gmail.com>
2019-01-13 22:52:21 +0200

    manager update

--- server/dataManager/Mongo/index.js
+++ server/dataManager/Mongo/index.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { Service, Enviorment, Config } from './schemas';
 
 export default class MongoManager {
     constructor(connectionString) {
@@ -7,4 +8,37 @@ export default class MongoManager {
         mongoose.connect(this.connectionString);
         mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
     }
+
+    createService({ name, description, environments }) {
+        //var service = new Service({name: name, description: description, })
+    }
+
+    updateConfig(serviceId, environmentName, data) {
+
+    }
+
+    getConfigs(serviceId, env) {
+
+    }
+
+    getConfig(serviceId, env) {
+
+    }
+
+    getAllEnv() {
+
+    }
+
+    //#region privates
+
+    _createEnviorment({ name, config }) {
+
+    }
+
+    async _createConfig({ data, key }) {
+        let config = new Config({ data: data, version: key });
+        return await config.save()._id;
+    }
+
+    //#endregion
 }
