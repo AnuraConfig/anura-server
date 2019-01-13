fcaf3736a057a287f637c7fbc394e6487e380182
VainEyal <eyalvainer@gmail.com>
2019-01-14 00:48:52 +0200

    get all works

--- server/dataManager/Mongo/index.js
+++ server/dataManager/Mongo/index.js
@@ -9,8 +9,15 @@ export default class MongoManager {
         mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
     }
 
-    createService({ name, description, environments }) {
-        //var service = new Service({name: name, description: description, })
+    async createService({ name, description, environments }) {
+        let enviormentIds = [];
+        for(let enviorment of environments) {
+            let newEnviorment = await this._createEnviorment(enviorment);
+            enviormentIds.push(newEnviorment._id);
+        }
+
+        var service = new Service({name: name, description: description, enviormentIds: enviormentIds});
+        return service.save();
     }
 
     updateConfig(serviceId, environmentName, data) {
@@ -26,18 +33,23 @@ export default class MongoManager {
     }
 
     getAllEnv() {
-
+        return Service.find({})
+            .populate({path: 'enviormentIds', populate: {path: 'configIds'}})
+            .exec();
     }
 
     //#region privates
 
-    _createEnviorment({ name, config }) {
+    async _createEnviorment({ name, config }) {
+        let newConfig = await this._createConfig(config);
 
+        let enviorment = new Enviorment({name: name, configIds: [newConfig._id]});
+        return enviorment.save();
     }
 
     async _createConfig({ data, key }) {
         let config = new Config({ data: data, version: key });
-        return await config.save()._id;
+        return config.save();
     }
 
     //#endregion
