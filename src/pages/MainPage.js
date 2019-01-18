83556ac6bb1395e5c85c2066744e13692976b4fe
Hen Shalom <xdrakolax@gmail.com>
2019-01-19 00:05:28 +0200

    remove old material style

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,28 +1,10 @@
 import React from "react";
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
 import ConfigList from '../components/ConfigLists/ConfigList'
 import FileViewer from '../components/FileViewer/FileViewer'
-import { withStyles } from '@material-ui/core/styles';
-import LoadingError from "../components/Common/LoadingError";
 
-const styles = theme => ({
-    root: {
-        width: "80%",
-        margin: " 50px auto",
-        height: "80vh",
-        padding: "0 20px"
-    },
-    grid: {
-        marginTop: '5px',
-        backgroundColor: theme.palette.background.paper,
-        flexGrow: 1,
-    }
-})
 
 class MainPage extends React.Component {
     render() {
-        const { classes } = this.props
         return (
             <div className="main">
                 <ConfigList />
@@ -32,4 +14,4 @@ class MainPage extends React.Component {
     }
 }
 
-export default withStyles(styles)(MainPage);
+export default MainPage;
