34ce03d40583adf56bc2a7c479cf8af7ef99d618
Hen Shalom <xdrakolax@gmail.com>
2019-01-19 00:43:44 +0200

    remove unfitting error image

--- src/components/Common/LoadingError.js
+++ src/components/Common/LoadingError.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ErrorImage from "./../../views/svg/error.png";
 
 export default class LoadingError extends React.PureComponent {
     constructor(props) {
@@ -10,7 +9,6 @@ export default class LoadingError extends React.PureComponent {
         return (
             <div className={"loadingError"}>
                 <div className={"loadingError__container"}>
-                    <img className={"loadingError__img"} src={ErrorImage}/>
                     <h2 className={"loadingError__header"}>Hey.</h2><p className={"loadingError__text"}>Sorry we
                     couldn't load the configuration.</p>
                 </div>
