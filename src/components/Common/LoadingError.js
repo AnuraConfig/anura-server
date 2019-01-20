92a54d0eabfa87dd1f32ed0a544b77159b1f1dd0
Hen Shalom <xdrakolax@gmail.com>
2019-01-20 12:04:35 +0200

    remove useless constructor

--- src/components/Common/LoadingError.js
+++ src/components/Common/LoadingError.js
@@ -1,10 +1,6 @@
 import React from "react";
 
 export default class LoadingError extends React.PureComponent {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return (
             <div className={"loadingError"}>
