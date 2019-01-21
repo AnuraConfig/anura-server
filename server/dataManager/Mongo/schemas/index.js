1b0c5714ad3afb32a8a41234f36c3fe08b5e9263
VainEyal <eyalvainer@gmail.com>
2019-01-21 23:06:17 +0200

    removed semi's from scehma

--- server/dataManager/Mongo/schemas/index.js
+++ server/dataManager/Mongo/schemas/index.js
@@ -4,21 +4,21 @@ import mongoose from 'mongoose';
 const ConfigSchema = new mongoose.Schema({
     version: Number,
     data: String
-});
+})
 
 const EnviormentScehma = new mongoose.Schema({
     name: String,
     configs: [{type: mongoose.Schema.Types.ObjectId, ref: 'configs'}]
-});
+})
 
 const ServiceSchema = new mongoose.Schema({
     name: String,
     description: String,
     environments: [{type: mongoose.Schema.Types.ObjectId, ref: 'environments'}]
-});
+})
 
 module.exports = {
     Config: mongoose.model('configs', ConfigSchema),
     Enviorment: mongoose.model('environments', EnviormentScehma),
     Service: mongoose.model('services', ServiceSchema)
-};
+}
