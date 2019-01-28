a9d93fea04606519b6c9b73a8022477c5477e167
Hen Shalom <xdrakolax@gmail.com>
2019-01-28 23:10:55 +0200

    change defult config to string

--- src/Constant/constants.js
+++ src/Constant/constants.js
@@ -1,2 +1,4 @@
 export const requestTypes = ['GET', 'POST', 'DELETE', 'PUT']
-export const INIT_CONFIG = { "Inset you config": "Here" }
+export const INIT_CONFIG = `{ 
+    "Inset you config": "Here"
+}`
