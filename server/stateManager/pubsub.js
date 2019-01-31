52ec713650e1019b5b3f8e81496bcca530ef84bb
VainEyal <eyalvainer@gmail.com>
2019-01-31 14:05:22 +0200

    moved pubsub initer to use the types

--- server/stateManager/pubsub.js
+++ server/stateManager/pubsub.js
@@ -1,11 +1,10 @@
 import { config } from '../constants/configs';
+import * as pubsubTypes from '../constants/pubsubTypes';
 import redisAdaptor from 'socket.io-redis';
-
-const REDIS = "REDIS"
 
 export function initializePubsub(io) {
     switch(config.PUB_SUB.TYPE) {
-        case(REDIS):
+        case(pubsubTypes.REDIS):
             io.adapter(redisAdaptor(config.PUB_SUB.HOST))
             break
         default:
