ef38863d588c72a63d070f7213aefa8acb8d9ff8
Hen Shalom <xdrakolax@gmail.com>
2019-02-01 17:31:08 +0200

    add config setting context

--- src/Context/ContextProvider.js
+++ src/Context/ContextProvider.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react'
 import SearchContext from './SearchContext'
 import SelectFileContextProvider from './SelectFileContext'
+import ConfigSettingsContextProvider from './ConfigTypeContext'
 
 export default class ContextProvider extends Component {
     render() {
         return (
-            <SelectFileContextProvider>
-                <SearchContext>
-                    {this.props.children}
-                </SearchContext>
-            </SelectFileContextProvider>
+            <ConfigSettingsContextProvider>
+                <SelectFileContextProvider>
+                    <SearchContext>
+                        {this.props.children}
+                    </SearchContext>
+                </SelectFileContextProvider>
+            </ConfigSettingsContextProvider>
         )
     }
 }
--- src/Context/Contexts.js
+++ src/Context/Contexts.js
@@ -2,3 +2,4 @@ import React from "react"
 
 export const SelectFileContext = React.createContext({ envName: "", serviceId: "" });
 export const SearchContext = React.createContext({ text: "", changeText: () => { } });
+export const ConfigSettingsContext = React.createContext({ settings: {}, toggleMenu: () => { }, changeSettings: () => { } });
