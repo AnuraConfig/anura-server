d8e87e0c49712d43393addf32ec9bef483ec8c23
VainEyal <eyalvainer@gmail.com>
2019-02-05 23:14:13 +0200

    added toast and removed copies state parameter

--- src/components/ConfigLists/ServiceItem.js
+++ src/components/ConfigLists/ServiceItem.js
@@ -6,6 +6,7 @@ import EnvList from './EnvList'
 import Divider from '@material-ui/core/Divider';
 import { SelectFileContext } from '../../Context/Contexts'
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { toast } from 'react-toastify';
 // icons
 import Apps from '@material-ui/icons/Apps';
 import FileCopyOutlined from '@material-ui/icons/FileCopyOutlined'; 
@@ -28,8 +29,7 @@ function generateTitle(heightLightText, text) {
 
 class ServiceItem extends React.Component {
     state = {
-        open: false,
-        copied: false
+        open: false
     };
 
     handleClick = (clickFile) => {
@@ -48,10 +48,10 @@ class ServiceItem extends React.Component {
                             <ListItemIcon>
                                 <Apps />
                             </ListItemIcon>
-                            <CopyToClipboard text={'66'}
+                            <CopyToClipboard text={id.toString()}
                                 onCopy={() => {
-                                    this.setState({copied: true,  open: !this.state.open});
-                                    console.log("copied")
+                                    this.setState({ open: !this.state.open });
+                                    toast.success('Copied value', {autoClose: 2000})
                                     }}>
                                 <FileCopyOutlined />
                             </CopyToClipboard>
