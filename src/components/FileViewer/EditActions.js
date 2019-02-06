71f3a05dfc4d0ef61ca0b2594356dd4b29634d2a
Hen Shalom <xdrakolax@gmail.com>
2019-02-06 21:29:53 +0200

    moved context outside

--- src/components/FileViewer/EditActions.js
+++ src/components/FileViewer/EditActions.js
@@ -3,21 +3,16 @@ import Setting from '@material-ui/icons/Settings'
 import Button from '@material-ui/core/Button';
 import { EditActions as styles } from './styles'
 import { withStyles } from '@material-ui/core';
-import { ConfigSettingsContext } from '../../Context/Contexts';
 import IconButton from '@material-ui/core/IconButton';
 
 class EditActions extends Component {
     render() {
-        const { classes, changeData, updateConfig, serviceId, envName } = this.props
+        const { classes, changeData, updateConfig, serviceId, envName, toggleMenu, settings } = this.props
         return (
             <div>
-                <ConfigSettingsContext.Consumer>
-                    {({ toggleMenu }) =>
-                        (<IconButton className={classes.settingButton} aria-label="Edit" onClick={toggleMenu}>
-                            <Setting />
-                        </IconButton>)
-                    }
-                </ConfigSettingsContext.Consumer>
+                <IconButton className={classes.settingButton} aria-label="Edit" onClick={toggleMenu}>
+                    <Setting />
+                </IconButton>
                 <Button variant="outlined" color="secondary" className={classes.cancel}
                     onClick={this.cancelUpdate}>
                     Cancel
