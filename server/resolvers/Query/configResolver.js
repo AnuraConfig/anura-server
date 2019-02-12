2674a01ab6d087158ffac7212730e0c507c0107f
VainEyal <eyalvainer@gmail.com>
2019-02-12 10:56:20 +0200

    removed data manger coupling in config resolver

--- server/resolvers/Query/configResolver.js
+++ server/resolvers/Query/configResolver.js
@@ -1,8 +1,6 @@
-import dataManager from '../../dataManager'
-
-export function getConfigs(obj, args) {
-    return dataManager.manager.getConfigs(args.serviceId, args.environment, args.raw)
+export function getConfigs(root, args, ctx, info) {
+    return ctx.dataSources.getConfigs(args.serviceId, args.environment, args.raw)
 }
-export function latestConfig(obj, args) {
-    return dataManager.manager.getConfig(args.serviceId, args.environment, args.raw)
+export function latestConfig(root, args, ctx, info) {
+    return ctx.dataSources.getConfig(args.serviceId, args.environment, args.raw)
 }
