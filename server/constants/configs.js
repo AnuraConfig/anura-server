24663e7341df8033d62db324515931dc85eb623e
Hen Shalom <xdrakolax@gmail.com>
2019-02-15 09:43:16 +0200

    add event subscribtion to config

--- server/constants/configs.js
+++ server/constants/configs.js
@@ -10,6 +10,17 @@ class Config {
         const file = fs.readFileSync(configLocation, 'utf8')
         const defaultConfig = YAML.safeLoad(file)
         this.config = defaultConfig
+        this.callbacks = []
+    }
+
+    subscribeCallback(callback) {
+        this.callbacks.push(callback)
+    }
+
+    activeCallback(config) {
+        for (let callback of this.callbacks) {
+            callback(config)
+        }
     }
 
     loadConfig(configs) {
@@ -20,6 +31,7 @@ class Config {
             configFileObject = YAML.parse(file)
         }
         config = Object.assign({}, defaultConfig, configFileObject, configs)
+        this.activeCallback(config)
         return config
     }
 
