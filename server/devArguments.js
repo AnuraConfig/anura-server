5812ee20168dbac85a1d86c7c17870187ab17add
Hen Shalom <xdrakolax@gmail.com>
2019-02-16 20:49:25 +0200

    create log dir if not exists

--- server/devArguments.js
+++ server/devArguments.js
@@ -1,4 +1,10 @@
 import path from "path"
+import fs from "fs"
+
+const logDir = path.join(__dirname, '../log')
+if (!fs.existsSync(logDir)) {
+    fs.mkdirSync(logDir)
+}
 
 export default {
     STORE_LOCATION: path.join(__dirname, '../'),
