17331dd1ea336caf1bbb9bd474f6f299644b3a56
VainEyal <eyalvainer@gmail.com>
2019-02-19 23:44:43 +0200

    added if for installand env variable

--- bin/scripts/post-install.js
+++ bin/scripts/post-install.js
@@ -1,12 +1,14 @@
-const fs = require("fs")
-const path = require("path")
+if(process.env.NODE_ENV !== "dev") {
+    const fs = require("fs")
+    const path = require("path")
 
-const { baseDirectory, configFileLocation } = require('./function')
+    const { baseDirectory, configFileLocation } = require('./function')
 
-if (!fs.existsSync(baseDirectory)) {
-    fs.mkdirSync(baseDirectory)
+    if (!fs.existsSync(baseDirectory)) {
+        fs.mkdirSync(baseDirectory)
+    }
+    const file = fs.readFileSync(path.join(__dirname, '../../defaultConfig.yaml'))
+    fs.writeFileSync(configFileLocation, file)
+
+    console.log("Welcome to Anura")
 }
-const file = fs.readFileSync(path.join(__dirname, '../../defaultConfig.yaml'))
-fs.writeFileSync(configFileLocation, file)
-
-console.log("Welcome to Anura")
