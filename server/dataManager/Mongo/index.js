6eba9f3449fae118cb128ca622b7a91246e27193
Hen Shalom <xdrakolax@gmail.com>
2019-02-22 22:48:30 +0200

    add configConvertor to mongo connector

--- server/dataManager/Mongo/index.js
+++ server/dataManager/Mongo/index.js
@@ -1,13 +1,13 @@
 import mongoose from 'mongoose';
 import { Service, Environment, Config } from './schemas';
 import { config } from '../../constants/configs'
+import configConvertor from '../../configConvertor'
 
 
 export default class MongoManager {
     constructor(connectionString = config.MONGO_STORE) {
         console.log("WARNING THIS CONNECTOR IS BROKEN ON THIS VERSION DON'T USE IT ") //TODO: fix this connector and remove the message
         this.connectionString = connectionString
-
         mongoose.connect(this.connectionString)
         mongoose.connection.on('error', console.error.bind(console, 'connection error:'))
     }
@@ -18,7 +18,7 @@ export default class MongoManager {
             promises.push(this._createEnvironment(environment))
         }
 
-        let newEnvironments = await Promise.all(promises)
+        let newEnvironments = await Promise.all(promises).catch(e => { throw e })
         var service = new Service({
             name: name,
             description: description,
@@ -27,11 +27,13 @@ export default class MongoManager {
         return service.save()
     }
 
-    async updateConfig(serviceId, environmentName, data) {
+    async updateConfig(serviceId, environmentName, data, type) {
+        validConfigType(data, type)
         const service = await this._findService(serviceId, environmentName)
         let environment = await Environment.findById(service.environments[0].id).exec()
         let newConfig = new Config({
-            data: data,
+            data,
+            type,
             version: environment.configs.length
         })
         newConfig = await newConfig.save()
@@ -39,7 +41,7 @@ export default class MongoManager {
         return environment.save()
     }
 
-    async getConfigs(serviceId, env) {
+    async getConfigs(serviceId, env, raw) {
         const service = await Service
             .find({
                 _id: mongoose.Types.ObjectId(serviceId)
@@ -56,12 +58,12 @@ export default class MongoManager {
             .exec()
         return {
             name: service[0].environments[0].name,
-            configs: service[0].environments[0].configs
+            configs: this._prosesConfigs(service[0].environments[0].configs, raw)
         }
     }
 
-    async getConfig(serviceId, env) {
-        const allConfigs = await this.getConfigs(serviceId, env)
+    async getConfig(serviceId, env, raw) {
+        const allConfigs = await this.getConfigs(serviceId, env, raw)
         return allConfigs.configs.sort(item => item.version).slice(-1)[0].data
     }
 
@@ -77,10 +79,15 @@ export default class MongoManager {
     }
 
     //#region privates
-
+    _prosesConfigs(configs, raw) {
+        if (raw)
+            return configs;
+        return configs.map(config => {
+            config.data = JSON.stringify(configConvertor.getObject(config.data, config.type))
+        })
+    }
     async _createEnvironment({ name, config }) {
         let newConfig = await this._createConfig(config)
-
         let environment = new Environment({
             name: name,
             configs: [newConfig._id]
@@ -89,8 +96,10 @@ export default class MongoManager {
         return environment.save()
     }
 
-    async _createConfig({ data, key }) {
+    async _createConfig({ data, type, key }) {
+        validConfigType(data, type)
         let config = new Config({
+            type,
             data: data,
             version: key || 0
         })
