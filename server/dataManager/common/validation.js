5c4aa86977a9bbbc8661b1286acb86ef9bfe0053
Hen Shalom <xdrakolax@gmail.com>
2019-02-23 19:24:00 +0200

    refactir validation to write logs

--- server/dataManager/common/validation.js
+++ server/dataManager/common/validation.js
@@ -1,6 +1,15 @@
 import configConvertor from '../../configConvertor'
 
-export function validConfigType(data, type) {
-    if (!configConvertor.typeDic[type]) throw new Error(`no such type:  ${type}`)
-    if (!configConvertor.isValid(data, type)) throw new Error(`config is not a valid config from type  ${type}`)
+export function validConfigType(data, type, logger) {
+    if (!configConvertor.typeDic[type]) logAndThrow(`no such type:  ${type}`, logger)
+    if (!configConvertor.isValid(data, type)) logAndThrow(`config is not a valid config from type  ${type}`, logger)
+}
+
+export function logAndThrow(message, logger) {
+    if (logger)
+        if (typeof logger === "object")
+            logger.log(message, "error")
+        else
+            logger(message, "error")
+    throw new Error(message)
 }
