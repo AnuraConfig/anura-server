4a98387441587f0f7eaf77e02c47f09945477746
Hen Shalom <xdrakolax@gmail.com>
2019-03-03 18:47:21 +0200

    remove unused imports

--- src/components/ConfigLists/ServiceItem.js
+++ src/components/ConfigLists/ServiceItem.js
@@ -5,11 +5,8 @@ import ListItemText from '@material-ui/core/ListItemText';
 import EnvList from './EnvList'
 import Divider from '@material-ui/core/Divider';
 import { SelectFileContext } from '../../Context/Contexts'
-import { CopyToClipboard } from 'react-copy-to-clipboard';
-import { toast } from 'react-toastify';
 // icons
 import Apps from '@material-ui/icons/Apps';
-import FileCopyOutlined from '@material-ui/icons/FileCopyOutlined'; 
 import ExpandMore from '@material-ui/icons/ExpandMore';
 import ExpandLess from '@material-ui/icons/ExpandLess';
 
