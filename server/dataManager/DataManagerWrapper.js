8ba8b194eb5c3e9ff3b96bb4ea3f761060e291b6
Hen Shalom <xdrakolax@gmail.com>
2019-03-30 22:41:00 +0300

    fix data manager wrapper

--- server/dataManager/DataManagerWrapper.js
+++ server/dataManager/DataManagerWrapper.js
@@ -14,31 +14,31 @@ export default class DataManagerWrapper {
     }
     async createService({ name, description, environments }) {
         this._log(`create service ${name}`)
-        this.connector.createService({ name, description, environments })
+        return this.connector.createService({ name, description, environments })
     }
     async updateService(updatedService, originalName) {
         this._log(`get service, serviceName:${serviceName}`)
-        this.connector.updateService(updatedService, originalName)
+        return this.connector.updateService(updatedService, originalName)
     }
     async updateConfig(serviceName, environmentName, data, type = "TEXT") {
         this._log(`update service, serviceName:${originalName}`)
-        this.connector.updateConfig(serviceName, environmentName, data, type)
+        return this.connector.updateConfig(serviceName, environmentName, data, type)
     }
     async getService(serviceName, raw, lastConfig) {
         this._log(`update config, serviceName:${serviceName}, environmentName:${environmentName}`)
-        this.connector.getService(serviceName, raw, lastConfig)
+        return this.connector.getService(serviceName, raw, lastConfig)
     }
     async getConfigs(serviceName, env, raw) {
         this._log(`get configs serviceName:${serviceName}, environmentName:${env}`)
-        this.connector.getConfigs(serviceName, env, raw)
+        return this.connector.getConfigs(serviceName, env, raw)
     }
     async getConfig(serviceName, env, raw) {
         this._log(`get configs serviceName:${serviceName}, environmentName:${env}`)
-        this.connector.getConfig(serviceName, env, raw)
+        return this.connector.getConfig(serviceName, env, raw)
     }
     async getAllEnv() {
         this._log(`get all environment`)
-        this.connector.getAllEnv()
+        return this.connector.getAllEnv()
     }
     _log = (message, level = "info") => {
         this.logger.log({ message: `${this.connectorName} Manger: ${message} `, level })
