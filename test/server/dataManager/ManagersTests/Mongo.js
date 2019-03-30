d82a6c809e055f24337adeabcbdafaf15f885493
Hen Shalom <xdrakolax@gmail.com>
2019-03-30 22:32:53 +0300

    update tests

--- test/server/dataManager/ManagersTests/Mongo.js
+++ test/server/dataManager/ManagersTests/Mongo.js
@@ -1,10 +1,10 @@
 import chai from "chai"
 import { Service, Environment, Config } from '../../../../server/dataManager/Mongo/schemas';
 import mongoose from 'mongoose';
-import MongoManager from "../../../../server/dataManager/Mongo";
-import { StateMangerMock } from "./mocks/stateManger"
-//import dataManagerRequestTests from "./common/dataManagerRequestTests"
+import MongoManager from "../../../../server/dataManager/Mongo"
 import newService from "./mocks/newService"
+import { logMock } from "./mocks/generalMocks"
+//import dataManagerRequestTests from "./common/dataManagerRequestTests"
 
 mongoose.models = {};
 mongoose.modelSchemas = {};
@@ -14,10 +14,13 @@ const expect = chai.expect
 describe("Mongo data manager", function () {
     describe("check requests", function () {
         before(function (done) {
-            this.dataManager = new MongoManager("mongodb://localhost:27017/test", { log: () => { } }, StateMangerMock,
-                () => {
+            this.dataManager = new MongoManager({
+                connectionString: "mongodb://localhost:27017/test",
+                log: logMock,
+                callback: () => {
                     done()
-                })
+                }
+            })
         })
         beforeEach(async function () {
             await this.dataManager.createService(newService)
