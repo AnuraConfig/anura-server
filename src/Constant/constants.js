e0901589b7c78eda7225b09a146cfc362ff1637f
Hen Shalom <xdrakolax@gmail.com>
2019-07-13 14:25:51 +0300

    fix typo

--- src/Constant/constants.js
+++ src/Constant/constants.js
@@ -1,5 +1,5 @@
 export const requestTypes = ['GET', 'POST', 'DELETE', 'PUT']
 export const INIT_CONFIG = `{ 
-    "Inset you config": "Here"
+    "Insert you config": "Here"
 }`
 export const CONFIG_TYPE = ['JSON', 'YAML', 'TEXT']
