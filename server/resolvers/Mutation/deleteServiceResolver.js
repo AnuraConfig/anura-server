b23cc58d5fe4e922f0833565162ffdd3d2a451d2
Hen Shalom <xdrakolax@gmail.com>
2019-08-18 23:32:50 +0300

    Revert "Revert "Feature/better error logs""

--- server/resolvers/Mutation/deleteServiceResolver.js
+++ server/resolvers/Mutation/deleteServiceResolver.js
@@ -1,15 +1,15 @@
+import logger from '../../utils/logger'
+
 export default function (root, args, ctx, info) {
     try {
         const { serviceName } = args
         ctx.dataSources.deleteService(serviceName)
         return {
             success: true
-        };
+        }
     } catch (e) {
-        return {
-            success: false,
-            error: e.message
-        };
+        logger.log({ level: "error", message: e.message })
+        return { success: false, error: e.message }
     }
 
 }
--- server/resolvers/Query/getGlobalVariable.js
+++ server/resolvers/Query/getGlobalVariable.js
@@ -1,3 +1,10 @@
+import logger from '../../utils/logger'
+
 export default function (root, args, ctx, info) {
-  return ctx.dataSources.getGlobalVariable()
+  try {
+    return ctx.dataSources.getGlobalVariable()
+  } catch (e) {
+    logger.log({ level: "error", message: e.message })
+    throw e
+  }
 }
