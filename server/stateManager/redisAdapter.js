2d21e0a31e7287d1929d2bf0c0c42a706b964915
Hen Shalom <xdrakolax@gmail.com>
2019-09-06 13:32:12 +0300

    fix adapter

--- server/stateManager/redisAdapter.js
+++ server/stateManager/redisAdapter.js
@@ -1,9 +1,9 @@
 import redis from 'redis'
-import redisAdaptor from 'socket.io-redis';
+import redisAdapter from 'socket.io-redis';
 
 
 export default function initializeRedisAdapter(io, config) {
-  const { port, host, auth_pass } = config.PUB_SUB
+  const { port, host, auth_pass } = config
   const pub = redis.createClient(port, host, { auth_pass })
   const sub = redis.createClient(port, host, { auth_pass })
   io.adapter(redisAdapter({ pubClient: pub, subClient: sub }))
